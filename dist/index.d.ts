e52d135d9d01b7155f50b5b38f52446564f1b845
Alexander Sapountzis <alexs@mparticle.com>
2020-02-05 13:59:12 -0500

    Update version 0.1.1

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './data_planning/data_plan_event_validator';
-export * from './services/data_plan_service';
-export * from './validation/JSONSchemaValidator';
-export * from './types';
-export * from './utils/config';
